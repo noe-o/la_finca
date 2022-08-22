b29a24f6906b34e28828b65b2d6d064dc5ff33d6
Noemí Oteiza <oteizanoemi@gmail.com>
2022-08-22 10:04:36 -0300

    agrego JS a mi pagina web, para reservas

---
+++ index.js
@@ -0,0 +1,23 @@
+function reservar() {
+    let reserva = prompt("¿Desea hacer una reserva?");
+    if (reserva == "si" || reserva == "sí" || reserva == "yes") {
+        let cantidad = parseInt(prompt("¿Cuántos comensales?"));
+        if (cantidad > 20 || cantidad < 0 || cantidad == "") {
+            alert("La cantidad ingresada no es válida");
+        } else {
+            let nombre = prompt("Ingrese nombre");
+            let celular = prompt("Ingrese un celular de contacto");
+            let mail = prompt("Ingrese un e-mail");
+            let notas = prompt("Notas:");
+            console.log("Los comensales son: " + cantidad + ". Y las notas son: " + notas);
+            console.log("La reserva se realizó a nombre de: " + nombre + ", celular: " + celular + " y e-mail: " + mail);
+            let valorTotal=cantidad*3400;
+            console.log("La reserva tiene un valor total aproximado en efectivo de $" + valorTotal);
+            let p10 = valorTotal * 0.1;
+            console.log("Debe abonar una seña de: $" + p10 + ". Correspondiente al 10% del valor total de la reserva");
+        }
+    } else {
+        alert("Nos veremos la próxima");
+    }
+}
+reservar();
