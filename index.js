5ad762a94603b7e15908b048f4226c532fa3a28e
Noemí Oteiza <oteizanoemi@gmail.com>
2022-08-24 20:29:34 -0300

    agrego for en JS

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function reservar() {
-    let reserva = prompt("¿Desea hacer una reserva?");
-    if (reserva == "si" || reserva == "sí" || reserva == "yes") {
-        let cantidad = parseInt(prompt("¿Cuántos comensales?"));
+    let reserva = prompt("¿Desea hacer una reserva? (en caso afirmativo ingrese si)");
+    if (reserva == "si") {
+        let cantidad = parseInt(prompt("¿Cuántos comensales? (ingrese número)"));
+
         if (cantidad > 20 || cantidad < 0 || cantidad == "") {
             alert("La cantidad ingresada no es válida");
         } else {
@@ -11,13 +12,18 @@ function reservar() {
             let notas = prompt("Notas:");
             console.log("Los comensales son: " + cantidad + ". Y las notas son: " + notas);
             console.log("La reserva se realizó a nombre de: " + nombre + ", celular: " + celular + " y e-mail: " + mail);
-            let valorTotal=cantidad*3400;
+            let valorTotal = cantidad * 3400;
             console.log("La reserva tiene un valor total aproximado en efectivo de $" + valorTotal);
             let p10 = valorTotal * 0.1;
             console.log("Debe abonar una seña de: $" + p10 + ". Correspondiente al 10% del valor total de la reserva");
+
         }
     } else {
         alert("Nos veremos la próxima");
     }
 }
-reservar();
+
+
+for (let i = 0; i < 2; i++) {
+    reservar();
+}
