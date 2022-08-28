7a5ed206e1cefbf0ebc6a3ec37438a6eea99e585
Noemí Oteiza <oteizanoemi@gmail.com>
2022-08-28 20:30:22 -0300

    modifico para el complementario CLASE 6

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 function reservar() {
-    let reserva = prompt("¿Desea hacer una reserva? (en caso afirmativo ingrese si)");
-    if (reserva == "si") {
-        let cantidad = parseInt(prompt("¿Cuántos comensales? (ingrese número)"));
-
-        if (cantidad > 20 || cantidad < 0 || cantidad == "") {
-            alert("La cantidad ingresada no es válida");
-        } else {
-            let nombre = prompt("Ingrese nombre");
-            let celular = prompt("Ingrese un celular de contacto");
-            let mail = prompt("Ingrese un e-mail");
-            let notas = prompt("Notas:");
-            console.log("Los comensales son: " + cantidad + ". Y las notas son: " + notas);
-            console.log("La reserva se realizó a nombre de: " + nombre + ", celular: " + celular + " y e-mail: " + mail);
-            let valorTotal = cantidad * 3400;
-            console.log("La reserva tiene un valor total aproximado en efectivo de $" + valorTotal);
-            let p10 = valorTotal * 0.1;
-            console.log("Debe abonar una seña de: $" + p10 + ". Correspondiente al 10% del valor total de la reserva");
-
+    let quiere = prompt("¿Quiere reservar una mesa para el almuerzo del domingo? Responder: si/no")
+    if (quiere == "si") {
+        class Mesa {
+            constructor(nombre, celular, comensales) {
+                this.nombre = nombre;
+                this.celular = celular;
+                this.comensales = comensales;
+            }
         }
+        const mesas = [];
+        mesas.push(new Mesa(prompt("Nombre y apellido:"),
+            prompt("Celular de contacto:"),
+            parseInt(prompt("Número de comensales:"))));
+        console.log(mesas);
+        for (let i = 1; i < 3; i++) {
+            let otra = prompt("Si desea salir presione 0");
+            if (otra == 0) {
+                i = 2;
+            } else {
+                const mesas = [];
+                mesas.push(new Mesa(prompt("Nombre y apellido:"),
+                    prompt("Celular de contacto:"),
+                    parseInt(prompt("Número de comensales:"))));
+                console.log(mesas);
+            }
+        }
+    } else if (quiere == "no") {
+        alert("Ok, nos veremos la próxima");        
     } else {
-        alert("Nos veremos la próxima");
+        alert("No pudimos procesar su respuesta")
+
     }
 }
-
-
-for (let i = 0; i < 2; i++) {
-    reservar();
-}
+let resultado=reservar();
+console.log(resultado);
